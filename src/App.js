169df5e81ee31339b51f49239b1469f7057420c0
Josh Simmons <jcpsimmons@gmail.com>
2020-07-24 11:09:48 -0700

    fix netlify build settings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Header from "./components/Header/Header";
 import GameList from "./components/GameList/GameList";
 import EventPage from "./components/EventPage/EventPage";
-import LoadingSpinner from "./components/LoadingSpinner/LoadingSpinner";
 
 function App() {
   return (
